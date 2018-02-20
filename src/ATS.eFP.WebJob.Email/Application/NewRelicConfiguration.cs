a56612965a9d3962c58bdb3a732a7349af545d30
Justin Boundy <jboundy@advancedtech.com>
2018-02-20 15:58:33 -0600

    Updated configuration

---
+++ src/ATS.eFP.WebJob.Email/Application/NewRelicConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ATS.eFP.WebJob.Email.Application
+{
+    public static class NewRelicConfiguration
+    {
+        public static void Initialize()
+        {
+            NewRelic.Api.Agent.NewRelic.SetApplicationName(Settings.NewRelicConfig);
+            NewRelic.Api.Agent.NewRelic.StartAgent();
+        }
+    }
+}
--- src/ATS.eFP.WebJob.Email/Application/Settings.cs
+++ src/ATS.eFP.WebJob.Email/Application/Settings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using ATS.eFP.WebJob.Email.Application.Configuration;
 
 namespace ATS.eFP.WebJob.Email.Application
@@ -13,12 +8,14 @@ namespace ATS.eFP.WebJob.Email.Application
         public static EmailConfiguration EmailConfig { get; internal set; }
         public static WebJobAuthConfiguration WebJobAuthConfig { get; internal set; }
         public static LoggingConfiguration LoggingConfig { get; internal set; }
+        public static string NewRelicConfig { get; internal set; }
 
         public static void Initialize()
         {
             EmailConfig = ConfigurationManager.GetSection("EmailConfiguration") as EmailConfiguration;
             WebJobAuthConfig = ConfigurationManager.GetSection("WebJobAuthConfiguration") as WebJobAuthConfiguration;
             LoggingConfig = ConfigurationManager.GetSection("LoggingConfiguration") as LoggingConfiguration;
+            NewRelicConfig = ConfigurationManager.AppSettings["NewRelic.AppName"];
         }
     }
 }
